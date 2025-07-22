77e21ca2efdc47baa17a1512ef621abc567f7563
PioKl <kosiu342@gmail.com>
2025-07-22 17:22:58 +0200

    Add Experience and Projects

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,14 @@
 import "./styles/globals.css";
 import { Hero } from "./components/Hero";
+import Experience from "./components/Experience";
+import Projects from "./components/Projects";
 
 function App() {
   return (
     <>
       <Hero />
+      <Experience />
+      <Projects />
     </>
   );
 }
