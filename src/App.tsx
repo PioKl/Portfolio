389e68856a498e8babea704b9035c41134599683
PioKl <kosiu342@gmail.com>
2025-07-24 17:08:52 +0200

    Add Contact and Footer

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,8 @@ import "./styles/globals.css";
 import { Hero } from "./components/Hero";
 import Experience from "./components/Experience";
 import Projects from "./components/Projects";
+import Contact from "./components/Contact";
+import Footer from "./components/layout/Footer";
 
 function App() {
   return (
@@ -9,6 +11,8 @@ function App() {
       <Hero />
       <Experience />
       <Projects />
+      <Contact />
+      <Footer />
     </>
   );
 }
